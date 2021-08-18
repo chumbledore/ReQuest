8a701008af7ebf089d74d04460aeed529d677f7f
Dakota Chumbley <dchumbley23@gmail.com>
2021-08-17 20:24:28 -0500

    Token Creator service finished

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using API.DTOs;
+using API.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Models;
@@ -12,30 +13,36 @@ namespace API.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        private readonly TokenService _tokenService;
+        public AccountController(UserManager<AppUser> userManager,
+        
+        SignInManager<AppUser> signInManager, TokenService tokenService)
         {
+            _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
         }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<UserReturnDTO>> Login(LoginDTO loginDTO){
-            var user = await _userManager.FindByEmailAsync(loginDTO.Email);
+    [HttpPost("login")]
+    public async Task<ActionResult<UserReturnDTO>> Login(LoginDTO loginDTO)
+    {
+        var user = await _userManager.FindByEmailAsync(loginDTO.Email);
 
-            if(user == null) return Unauthorized();
+        if (user == null) return Unauthorized();
 
-            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDTO.Password, false);
+        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDTO.Password, false);
 
-            if(result.Succeeded)
+        if (result.Succeeded)
+        {
+            return new UserReturnDTO
             {
-                return new UserReturnDTO {
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    Token = "Eventually a token"
-                };
-            }
-
-            return Unauthorized();
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
+
+        return Unauthorized();
     }
+}
 }
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Models;
+
+namespace API.Services
+{
+    public class TokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                // Implement later, fix Seed Users
+                // new Claim(ClaimTypes.Name, user.FirstName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("temporary secret key"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
